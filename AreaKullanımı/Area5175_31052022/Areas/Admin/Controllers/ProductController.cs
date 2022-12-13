b470597b407d2435a9f6944646b213f53f770c07
Furkan DURGUN <furkanprogrammer@outlook.com>
2022-12-14 00:31:51 +0300

    created

---
+++ AreaKullanımı/Area5175_31052022/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Area5175_31052022.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AreaKullanımı/Area5175_31052022/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Area5175_31052022.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
